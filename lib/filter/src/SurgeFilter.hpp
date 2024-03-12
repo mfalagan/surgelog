3e98bd7198e0c05238d57e2f00ea74033d0cdfe7
falagan <falagan.mn@gmail.com>
2024-03-12 16:38:52 +0100

    Better filtering logic implemented

--- lib/filter/src/SurgeFilter.hpp
+++ lib/filter/src/SurgeFilter.hpp
@@ -2,15 +2,24 @@
 #define SURGEFILTER_H
 
 #include "Buffer.hpp"
+#include <cstdint>
+#include <cmath>
 
+// TODO: a LOT of testing in production env
 class SurgeFilter {
 private:
-    int16_t threshold;
-    uint32_t offset;
     Buffer *buffer;
+    uint32_t offset;
+    int16_t threshold;
+    float mean;             // exponential moving average
+    float stdev;            // forgetful welford's method
+    float ztrend;           // Trend of Z-scores capturing recent anomalies
+    float slow_alpha;
+    float fast_alpha;
+    float normalization;    // Normalization factor for Z-score combining
 
 public:
-    SurgeFilter(Buffer *buffer, uint32_t offset, int16_t threshold);
+    SurgeFilter(Buffer *buffer = nullptr, uint32_t offset = 0, int16_t threshold = 2.5, float slow_alpha = 1e-4f, float fast_alpha = 0.2f);
     ~SurgeFilter();
 
     bool filter(int16_t value);
